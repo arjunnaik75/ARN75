2e764bfb062cfee9cbae03f2f19d66d93e826428
Arjun Naik <157286129+arjunnaik75@users.noreply.github.com>
2026-01-28 18:49:54 +0530

    Add cleanup_users_groups.sh script for user/group cleanup
    Script to clean up user accounts and groups with UID/GID >= 1000, excluding specified accounts.

---
+++ cleanup_users_groups.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Warning !! Safety First !
+# Only for Non‑Production / LAB / Test Environment 
+# Before running such commands:
+# Make a backup of /etc/passwd, /etc/shadow, and /etc/group before deleting accounts.
+# Double‑check the account you want to keep
+
+# This will cleanup all user accounts and data having UID >= 1000 except accounts admin & arjun as excluded below: 
+for user in $(awk -F: '($3 >= 1000) {print $1}' /etc/passwd | egrep -v '^(admin|arjun)$'); do
+    sudo userdel -r "$user"
+done
+
+# This will cleanup all groups having GID >= 1000 except admingroup & arjun as excluded below:
+for group in $(awk -F: '($3 >= 1000) {print $1}' /etc/group | egrep -v '^(admingroup|arjun)$'); do
+    sudo groupdel "$group"
+done
