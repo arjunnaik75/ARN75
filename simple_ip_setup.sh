6ef0ba5736963f70a7ea30a6806dd42f2307b1d2
Arjun Naik <157286129+arjunnaik75@users.noreply.github.com>
2026-01-28 15:26:30 +0530

    Add script for static IP setup using nmcli
    This script sets up a static IP configuration for the network interface enp2s0, modifies DNS settings, and verifies the configuration.

---
+++ simple_ip_setup.sh
@@ -0,0 +1,23 @@
+# Find the interface like enp2s0
+nmcli | egrep 'eth|en'
+nmcli | grep interface
+
+# Modify connection with static IP
+nmcli con mod enp2s0 ipv4.addresses 192.168.1.100/24
+nmcli con mod enp2s0 ipv4.gateway 192.168.1.1
+nmcli con mod enp2s0 ipv4.dns "192.168.1.1 8.8.8.8 8.8.4.4 1.1.1.1"
+nmcli con mod enp2s0 ipv4.method manual
+nmcli con mod ens160 connection.autoconnect yes
+
+# Bring connection up
+nmcli con up enp2s0
+# Reload connection / Restart Netwotk Manager
+nmcli connection reload ens160
+systemctl restart NetworkManager
+
+# Verify
+ip a s enp2s0 > ip.txt && cat /etc/resolv.conf >> ip.txt
+cat ip.txt
+
+
+
